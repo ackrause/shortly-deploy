74329157208807e67067b56ba5ae9f3b9e691319
Andrew Krause <a.c.krause@gmail.com>
2014-05-14 15:55:55 -0700

    Moved database username, password, and host to environment variables

--- app/config.js
+++ app/config.js
@@ -4,9 +4,9 @@ var path = require('path');
 var db = Bookshelf.initialize({
   client: 'sqlite3',
   connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'shortlydb',
     charset: 'utf8',
     filename: path.join(__dirname, '../db/shortly.sqlite')
