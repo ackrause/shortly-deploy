d2de01d37d51abc22d826f835986aa25fc6d77ec
Andrew Krause <a.c.krause@gmail.com>
2014-05-15 01:40:20 -0700

    Link schema and model definition

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,20 @@
-var db = require('../config');
+var db = require('../config.js');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var LinkSchema = new db.Schema({
+  url: {type: String, required: true },
+  base_url: {type: String},
+  code: {type: String},
+  title: {type: String},
+  visits: {type: Number, default: 0}
 });
 
-module.exports = Link;
+LinkSchema.pre('save', function(next) {
+  var link = this;
+  var shasum = crypto.createHash('sha1');
+  shasum.update(link.get('url'));
+  link.set('code', shasum.digest('hex').slice(0,5));
+  next();
+});
+
+module.exports = db.model('Link', LinkSchema);
